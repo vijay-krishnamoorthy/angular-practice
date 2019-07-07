ae62a172937dce39797cefa299624799bc9bbf79
Vijay Krishnamoorthy <vijaytoinbox@gmail.com>
2019-07-07 13:08:27 +0530

    Navigation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
